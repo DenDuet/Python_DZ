a62115cd8d0b28864415ca5b9ef92b0cb60d62de
Denis <9581886@gmail.com>
2022-09-03 14:01:53 +0300

    Файлы первого ДЗ изменил решение в  задании 3

--- DZ1/dz3.py
+++ DZ1/dz3.py
@@ -6,27 +6,18 @@
 # - x=2; y=4-> 1
 # - x=-34; y=-30 -> 3
 
-def get_coordinate_X():
+def get_coordinate():
     while True:
         try:
-            X = int(input("Введите число X: "))
-            while X == 0:
-                print("Y должен быть больше 0!")
-                X = get_coordinate_X()            
-            return X
+            num = int(input())
+            while num == 0:
+                print("Число должно быть больше или меньше 0!")
+                num = int(input())
+                continue
+            return num
         except ValueError:
-            print("Вы ввели не число. Повторите ввод")
+            print("Вы ввели не число. Повторите ввод.")
 
-def get_coordinate_Y():
-    while True:
-        try:
-            Y = int(input("Введите число Y: "))
-            while Y == 0:
-                print("Y должен быть больше 0!")
-                Y = get_coordinate_Y()
-            return Y
-        except ValueError:
-            print("Вы ввели не число. Повторите ввод")
             
 def quarter(X,Y):
     if (X>0 and Y>0): print("Первая четверть [0 - ∞, 0 - ∞]")
@@ -34,7 +25,9 @@ def quarter(X,Y):
     elif (X<0 and Y<0): print("Третья четверть [-∞ - 0, -∞ - 0]")
     elif (X<0 and Y>0): print("Вторая четверть [-∞ - 0, 0 - ∞]")
 
-X = get_coordinate_X()
-Y = get_coordinate_Y()
+print("Введите число X отличное от 0: ")
+X = get_coordinate()
+print("Введите число Y отличное от 0: ")
+Y = get_coordinate()
 
 quarter(X,Y)
