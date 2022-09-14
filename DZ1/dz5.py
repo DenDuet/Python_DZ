8dce4faa540334cc920a6c97b741288a120f5a92
Denis <9581886@gmail.com>
2022-09-14 19:39:03 +0300

    Файлы первого изменен ДЗ

--- DZ1/dz5.py
+++ DZ1/dz5.py
@@ -7,17 +7,17 @@
 
 import math
 
-def get_coordinate():
+def get_coordinate(input_string):
     while True:
         try:
-            num = int(input("Введите координату: "))
+            num = int(input(input_string))
             return num
         except ValueError:
             print("Вы ввели не число. Повторите ввод")
             
-x1 = get_coordinate()
-y1 = get_coordinate()
-x2 = get_coordinate()
-y2 = get_coordinate()
+x1 = get_coordinate("Введите координату x первой точки: ")
+y1 = get_coordinate("Введите координату y первой точки: ")
+x2 = get_coordinate("Введите координату x второй точки: ")
+y2 = get_coordinate("Введите координату y второй точки: ")
 
 print(f"Расстояние между точкой A({x1},{y1}) и B({x2},{y2}) равно {round(math.sqrt((x2-x1)**2 + (y2-y1)**2),2)}")
